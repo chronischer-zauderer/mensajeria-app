e94cf959cb0701dbeed7146f8267bcb3a0a9a923
Julio Cardona <163765285+chronischer-zauderer@users.noreply.github.com>
2025-11-09 12:45:42 -0500

    Feature/route security (#10)
    * little changes
    
    * cambio back
    
    * vista inicial del home en progreso (#3)
    
    vista de login en progreso
    vista de register en progreso
    
    * cors
    
    * route security
    
    * CI backend
    
    * CI backend cambios
    
    * CI backend cambios
    
    * CI backend cambios
    
    ---------
    
    Co-authored-by: Hermis999 <hermisjulian555@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,8 +10,8 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type : 'postgres',
-      host : 'localhost',
-      port : 5433,
+      host : 'postgres',
+      port : 5432,
       username : 'admin',
       password : 'admin123@',
       database : 'msbd',
